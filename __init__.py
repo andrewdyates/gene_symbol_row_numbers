6e0bc4cff84ef11d8128389cbd24d5301cd61325
Andrew Yates <andrewyates.name@gmail.com>
2012-08-23 16:12:44 -0400

    made into module

--- __init__.py
+++ __init__.py
@@ -1,3 +1,64 @@
 #!/usr/bin/python
 from geo_api import *
+from lab_util import *
+import sys, os
+
+
+def fname_is_tab(fname):
+  """Is filename tab-delimited text based on file extension?"""
+  return (fname.rpartition('.')[2].lower() == 'tab')
+  
+def get_gpl(gpl_brief=None, gpl_data=None, is_tab=True):
+  """Return geo.GPL object from GPL file names.
+
+  Args:
+    gpl_brief: str of file path to GPL file meta data brief (as from geo_downloader)
+    gpl_data: str of file path to GPL row descriptions IN DATA ROW ORDER (as from geo_downloader)
+      may either be .tab for SOFT text file format
+    is_tab: bool if gpl_data is in .tab format
+  Returns:
+    geo_api.GPL of loaded row descriptions
+  """
+  gpl = LocalGPL(fname_brief=gpl_brief, fname_data=gpl_data, data_is_tab=is_tab)
+  gpl.load()
+  return gpl
+
+def load_varlist(fp):
+  s = [s.partition('\t')[0].strip('\n') for s in fp]
+  return s
+
+def assert_row_alignment(M, varlist, gpl):
+  """Assert that GPL row descriptions and row id list aligns to data rows."""
+  assert np.size(M, 0) == len(gpl.row_desc) == len(gpl.probe_list) == len(gpl.probe_idx_map), \
+      " != ".join((np.size(M, 0), len(gpl.row_desc), len(gpl.probe_list), len(gpl.probe_idx_map)))
+  for i in xrange(np.size(M, 0)):
+    s = gpl.probe_list[i]
+    assert s == varlist[i]
+    assert gpl.probe_idx_map[s] == i
+    assert s in gpl.row_desc
+
+def mean_expression_threshold(means, percentile):
+  """Compute mean expression threshold above a fractional percentile."""
+  threshold = np.sort(means)[int(round(np.size(means)*percentile))]
+  return threshold
+
+def gene_sym_idxs(gpl=None, means=None, threshold=None):
+  """Return row numbers of unique gene symbols with maximum mean expression above percentile.
+
+  If threshhold is None, do not use threshhold.
+  """
+  assert gpl is not None and means is not None
+  symbols = {} # {str=>int} of gene_symbol=>row_number
+  for i in xrange(len(gpl.probe_list)):
+    if threshold is not None and means[i] <= threshold:
+      continue
+    row_id = gpl.probe_list[i]
+    gene_sym = gpl.get_column(row_id, 'GENE_SYMBOL')
+    if gene_sym is None:
+      continue
+    else:
+      # Add to symbol list if new symbol or previous mean is less than this mean.
+      if gene_sym not in symbols or means[symbols[gene_sym]] < means[i]:
+        symbols[gene_sym] = i
+  return symbols
 
--- script.py
+++ script.py
@@ -5,34 +5,34 @@ Load GPL file in row order.
 
 EXAMPLE USE:
   export STUDY_DIR=$HOME/Dropbox/biostat/study_data/GSE7307
-  python script.py outdir=$HOME/Desktop gpl_brief=$STUDY_DIR/GSE7307_GPL570.gpl_brief.txt gpl_data=$STUDY_DIR/GSE7307_GPL570.probes.tab study_data=$STUDY_DIR/GSE7307_GPL570.normed.masked.pkl varlist_fname=$STUDY_DIR/GSE7307_GPL570.varlist.txt
+  python script.py outdir=$HOME/Desktop gpl_brief=$STUDY_DIR/GSE7307_GPL570.gpl_brief.txt gpl_data=$STUDY_DIR/GSE7307_GPL570.probes.tab study_data=$STUDY_DIR/GSE7307_GPL570.normed.masked.pkl varlist_fname=$STUDY_DIR/GSE7307_GPL570.varlist.txt percentile=0.25
 """
-from geo_api import *
-from lab_util import *
+import sys, os
 import cPickle as pickle
 from lab_util.tab_to_npy import *
-import sys, os
+from lab_util import masked_npy_to_tab
+from __init__ import *
 
 def main(outdir=None, gpl_brief=None, gpl_data=None, study_data=None, varlist_fname=None, percentile=0.25):
   assert outdir and gpl_brief and gpl_data and study_data
   percentile = float(percentile)
   assert percentile >= 0
   if not os.path.exists(outdir):
+    print "Creating output directory %s..." % (outdir)
     make_dir(outdir)
 
   # Load GPL.
   print "Loading GPL definition from files %s and %s..." % (gpl_brief, gpl_data)
-  data_is_tab = (gpl_data.rpartition('.')[2].lower() == 'tab')
-  if data_is_tab:
+  gpl_is_tab = fname_is_tab(gpl_data)
+  if gpl_is_tab:
     print "Loading GPL data as tab file..."
   else:
     print "Loading GPL data as SOFT text file..."
-  gpl = LocalGPL(fname_brief=gpl_brief, fname_data=gpl_data, data_is_tab=data_is_tab)
-  gpl.load()
+  gpl = get_gpl(gpl_brief, gpl_data, gpl_is_tab)
   print "Loaded %d row descriptions." % len(gpl.row_desc)
 
-  # Load Data.
-  data_is_tab = (study_data.rpartition('.')[2].lower() == 'tab')
+  # Load data.
+  data_is_tab = fname_is_tab(study_data)
   if data_is_tab:
     print "Loading study data file %s as tab file to numpy.MaskedArray..." % (study_data)
     M, varlist = tab_to_npy(study_data)
@@ -40,52 +40,43 @@ def main(outdir=None, gpl_brief=None, gpl_data=None, study_data=None, varlist_fn
     assert varlist_fname, "varlist_fname parameter must be set to variable list"
     print "Loading study data file %s as pickled numpy.MaskedArray..." % (study_data)
     M = pickle.load(open(study_data))
-    varlist = [s.partition('\t')[0].strip('\n') for s in open(varlist_fname)]
+    varlist = load_varlist(open(varlist_fname))
     assert len(varlist) == np.size(M, 0)
 
-  # Assert that GPL row descriptions and row id list aligns to data rows.
-  print "Verifying row ID alignment..."
-  print np.size(M, 0), len(gpl.row_desc), len(gpl.probe_list), len(gpl.probe_idx_map)
-  assert np.size(M, 0) == len(gpl.row_desc) == len(gpl.probe_list) == len(gpl.probe_idx_map)
-  for i in xrange(np.size(M, 0)):
-    s = gpl.probe_list[i]
-    assert s == varlist[i]
-    assert gpl.probe_idx_map[s] == i
-    assert s in gpl.row_desc
-  print "Row alignment verified."
+  print "Verifying row ID alignment between data and GPL row definitions..."
+  assert_row_alignment(M=M, varlist=varlist, gpl=gpl)
 
-  # Compute mean expression threshold.
+  # Compute mean row (probe) expression above percentile.
   print "Computing %.2f percentile mean row expression threshold." % (percentile)
   means = M.mean(1)
+  threshold = mean_expression_threshold(means, percentile)
   print "Mean expression distributions:"
   print "min: %.4f, max: %.4f, mean: %.4f, median: %.4f" % \
       (means.min(), means.max(), means.mean(), np.median(means))
-  threshold = np.sort(means)[int(round(np.size(means)*percentile))]
-  print "Minimum mean expression threshold above @%.2f percentile: %.4f" % (percentile, threshold)
-  
-  # Select gene symbols with greatest mean expression.
-  symbols = {} # {str=>int} of gene_symbol=>row_number
-  for i in xrange(np.size(M,0)):
-    if means[i] <= threshold:
-      continue
-    row_id = varlist[i]
-    gene_sym = gpl.get_column(row_id, 'GENE_SYMBOL')
-    if gene_sym is None:
-      continue
-    else:
-      # Add to symbol list if new symbol or previous mean is less than this mean.
-      if gene_sym not in symbols or means[symbols[gene_sym]] < means[i]:
-        symbols[gene_sym] = i
-  print "Selected %d unique gene symbols" % len(symbols)
+  print "Minimum mean expression threshold above %.2f percentile: %.4f" % (percentile, threshold)
 
-  # Output results
+  # Get dictionary of gene symbols to row numbers
+  d_symbols = gene_sym_idxs(gpl=gpl, means=means, threshold=threshold)
+  print "Selected %d unique gene symbols" % len(d_symbols)
+  idxs = sorted(d_symbols.values())
+
+  # Output index results.
   study_id = os.path.basename(gpl_data).partition('.')[0]
-  out_fname = os.path.join(outdir, "%s.symbol_rownums.gt%.2f.txt" % (study_id, percentile))
-  print "Printing sorted list as [row_num]\\t[probe ID]\\t[gene symbol]\\n to %s" % out_fname
-  fp = open(out_fname, "w")
-  for i in sorted(symbols.values()):
+  out_idx_fname = os.path.join(outdir, "%s.symbol_rownums.gt%.2f.txt" % (study_id, percentile))
+  print "Saving sorted idx list in line format '[row_num]\\t[probe ID]\\t[gene symbol]\\n' to %s" \
+      % (out_idx_fname)
+  fp = open(out_idx_fname, "w")
+  for i in idxs:
     fp.write("%d\t%s\t%s\n" % (i, varlist[i], gpl.get_column(varlist[i], 'GENE_SYMBOL')))
   fp.close()
 
+  # Save data array copy of only selected rows.
+  out_M_fname = os.path.join(outdir, "%s.gt%.2f.tab" % (os.path.basename(gpl_data), percentile))
+  print "Saving %d selected rows of data matrix as .tab format as %s" % (len(idxs), out_M_fname)
+  masked_npy_to_tab.npy_to_tab( \
+    M[idxs, :], open(out_M_fname, 'w'), varlist=[varlist[i] for i in idxs])
+
+  
 if __name__ == "__main__":
+  print sys.argv
   main(**dict([s.split('=') for s in sys.argv[1:]]))
